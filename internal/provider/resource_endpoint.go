4204c46afe83e5188ef7caaec6f87f543f594110
Dmitry Kisler <admin@dkisler.com>
2023-01-08 18:26:30 +0100

    Add the note about endpoints per branch.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- internal/provider/resource_endpoint.go
+++ internal/provider/resource_endpoint.go
@@ -14,7 +14,12 @@ import (
 
 func resourceEndpoint() *schema.Resource {
 	return &schema.Resource{
-		Description:   "Project Endpoint. See details: https://neon.tech/docs/manage/endpoints/",
+		Description: `Project Endpoint. See details: https://neon.tech/docs/manage/endpoints/
+
+!> Note that "read_write" type is only supported yet, and a single "read_write" endpoint is permitted per branch. ` +
+			`It means that no additional endpoints can be provisioned for branches with existing endpoints.` +
+			`It also means that no endpoints can be created for branches provisioned with this terraform provider ` +
+			`because every branch has the default endpoint attached.`,
 		SchemaVersion: versionSchema,
 		Importer: &schema.ResourceImporter{
 			StateContext: resourceEndpointImport,
