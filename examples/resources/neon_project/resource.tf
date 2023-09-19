8d7af82acc7ddc2cd13f42b163266f8610b65093
Dmitry Kisler <admin@dkisler.com>
2023-09-20 00:39:57 +0200

    docu: update the project docu
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- examples/resources/neon_project/resource.tf
+++ examples/resources/neon_project/resource.tf
@@ -1,3 +1,36 @@
+### Default
+
 resource "neon_project" "example" {
   name = "foo"
+}
+
+### Turn off data retention
+
+resource "neon_project" "example" {
+  name                      = "foo"
+  history_retention_seconds = 0
+}
+
+### Set custom compute limits
+
+resource "neon_project" "example" {
+  name = "foo"
+
+  default_endpoint_settings {
+    autoscaling_limit_min_cu = 0.5
+    autoscaling_limit_max_cu = 1
+    suspend_timeout_seconds  = 10
+  }
+}
+
+### Define custom default branch
+
+resource "neon_project" "example" {
+  name = "foo"
+
+  branch {
+    name          = "bar"
+    database_name = "baz"
+    role_name     = "qux"
+  }
 }
