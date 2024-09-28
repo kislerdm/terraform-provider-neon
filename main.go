5efafa5016608f5137c7fa9894b910bbc20e97e6
Dmitry Kisler <admin@dkisler.com>
2024-09-28 12:59:47 +0200

    fix linter
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- main.go
+++ main.go
@@ -20,11 +20,8 @@ import (
 
 var (
 	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary
+	// to appropriate values for the compiled binary.
 	version string = "0.0.1"
-
-	// goreleaser can also pass the specific commit if you want
-	commit string = ""
 )
 
 func main() {
