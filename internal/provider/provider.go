d0b9c0a276c977288ea070817fcd84e466d3e29f
Dmitry Kisler <admin@dkisler.com>
2024-11-07 22:38:33 +0000

    refactor the retry logic out to a dedicated file
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -3,58 +3,13 @@ package provider
 import (
 	"context"
 	"math/rand"
-	"net/http"
 	"os"
-	"strconv"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	neon "github.com/kislerdm/neon-sdk-go"
 )
-
-type delay struct {
-	delay  time.Duration
-	maxCnt uint8
-}
-
-func (r *delay) Retry(
-	fn func(context.Context, *schema.ResourceData, interface{}) error,
-	ctx context.Context, d *schema.ResourceData, meta interface{},
-) diag.Diagnostics {
-	var i uint8
-	var err error
-	for i < r.maxCnt {
-		tflog.Debug(ctx, "API call attempt "+strconv.Itoa(int(i)))
-
-		switch e := fn(ctx, d, meta).(type) {
-		case nil:
-			return nil
-		case neon.Error:
-			tflog.Debug(ctx, "API call error code: "+strconv.Itoa(e.HTTPCode))
-			switch e.HTTPCode {
-			case 200:
-				return nil
-			case http.StatusTooManyRequests, http.StatusInternalServerError, http.StatusLocked:
-				tflog.Debug(ctx, "API call delay "+strconv.FormatInt(r.delay.Milliseconds(), 10)+" ms.")
-				err = e
-				i++
-				time.Sleep(r.delay)
-			default:
-				return diag.FromErr(e)
-			}
-		default:
-			return diag.FromErr(e)
-		}
-	}
-	return diag.FromErr(err)
-}
-
-var projectReadiness = delay{
-	delay:  1 * time.Second,
-	maxCnt: 120,
-}
 
 func init() {
 	rand.New(rand.NewSource(time.Now().Unix()))
