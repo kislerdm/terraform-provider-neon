25b97c0ff112aa3a43e75459cac1b57ca64ca1d3
Dmitry Kisler <admin@dkisler.com>
2026-08-01 20:51:06 +0200

    fix test cases for neon_database
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- provider/acc_database_test.go
+++ provider/acc_database_test.go
@@ -40,100 +40,85 @@ func TestRecreateDatabaseIfNotFound(t *testing.T) {
 		return projectNamePrefix + strconv.FormatInt(time.Now().UnixMilli(), 10)
 	}
 
-	t.Run("shall update the state if the database was deleted outside of terraform", func(t *testing.T) {
-		projectName := newProjectName()
-		config := fmt.Sprintf(`resource "neon_project" "this" {name = "%s"}
+	t.Run("shall yield non empty refresh plan if the database was deleted outside of terraform",
+		func(t *testing.T) {
+			projectName := newProjectName()
+			config := fmt.Sprintf(`resource "neon_project" "this" {name = "%s"}
 resource "neon_database" "this" {
 	project_id = neon_project.this.id
 	branch_id  = neon_project.this.default_branch_id
 	owner_name = neon_project.this.database_user
 	name       = "test"
 }`, projectName)
-		resource.Test(
-			t, resource.TestCase{
-				ProviderFactories: map[string]func() (*schema.Provider, error){
-					"neon": func() (*schema.Provider, error) {
-						return newAccTest(), nil
+			resource.Test(
+				t, resource.TestCase{
+					ProviderFactories: map[string]func() (*schema.Provider, error){
+						"neon": func() (*schema.Provider, error) {
+							return newAccTest(), nil
+						},
 					},
-				},
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check: resource.ComposeTestCheckFunc(
-							resource.TestCheckResourceAttr(
-								"neon_database.this",
-								"name", "test",
+					Steps: []resource.TestStep{
+						{
+							Config: config,
+							Check: resource.ComposeTestCheckFunc(
+								resource.TestCheckResourceAttr(
+									"neon_database.this",
+									"name", "test",
+								),
+								func(_ *terraform.State) error {
+									ref, err := readProjectInfo(client, projectName)
+									if err != nil {
+										return err
+									}
+									br, err := client.ListProjectBranches(ref.ID,
+										nil, nil, nil, nil, nil)
+									if err != nil {
+										return err
+									}
+
+									var branchID string
+									for _, branch := range br.Branches {
+										if branch.Default {
+											branchID = branch.ID
+										}
+									}
+
+									resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
+									if err != nil {
+										return err
+									}
+									assert.Len(t, resp.Databases, 2)
+
+									return nil
+								},
 							),
-							func(_ *terraform.State) error {
+						},
+						{
+							PreConfig: func() {
 								ref, err := readProjectInfo(client, projectName)
 								if err != nil {
-									return err
+									panic(err)
 								}
 								br, err := client.ListProjectBranches(ref.ID,
 									nil, nil, nil, nil, nil)
 								if err != nil {
-									return err
+									panic(err)
 								}
-
-								var branchID string
 								for _, branch := range br.Branches {
 									if branch.Default {
-										branchID = branch.ID
-									}
-								}
-
-								resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
-								if err != nil {
-									return err
-								}
-								assert.Len(t, resp.Databases, 2)
-
-								for _, db := range resp.Databases {
-									if db.Name == "test" {
-										_, err := client.DeleteProjectBranchDatabase(ref.ID, branchID, db.Name)
+										_, err = client.DeleteProjectBranchDatabase(ref.ID, branch.ID, "test")
 										if err != nil {
-											return err
+											panic(err)
 										}
 									}
 								}
-
-								return nil
 							},
-						),
+							RefreshState:       true,
+							ExpectNonEmptyPlan: true,
+						},
 					},
-					{
-						Config: fmt.Sprintf(`resource "neon_project" "this" {name = "%s"}`, projectName),
-						Check: resource.ComposeTestCheckFunc(
-							func(s *terraform.State) error {
-								ref, err := readProjectInfo(client, projectName)
-								if err != nil {
-									return err
-								}
-								br, err := client.ListProjectBranches(ref.ID,
-									nil, nil, nil, nil, nil)
-								if err != nil {
-									return err
-								}
-
-								var branchID string
-								for _, branch := range br.Branches {
-									if branch.Default {
-										branchID = branch.ID
-									}
-								}
-
-								resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
-								if err != nil {
-									return err
-								}
-								assert.Len(t, resp.Databases, 1)
-
-								return nil
-							}),
-					},
-				},
-			})
-	})
+				})
+		})
 
 	t.Run("shall destroy even if the database was deleted outside of terraform,", func(t *testing.T) {
 		projectName := newProjectName()
@@ -154,261 +139,31 @@ resource "neon_database" "this" {
 				Steps: []resource.TestStep{
 					{
 						Config: config,
-						Check: resource.ComposeTestCheckFunc(
-							resource.TestCheckResourceAttr(
-								"neon_database.this",
-								"name", "test",
-							),
-							func(_ *terraform.State) error {
-								ref, err := readProjectInfo(client, projectName)
-								if err != nil {
-									return err
-								}
-								br, err := client.ListProjectBranches(ref.ID,
-									nil, nil, nil, nil, nil)
-								if err != nil {
-									return err
-								}
-
-								var branchID string
-								for _, branch := range br.Branches {
-									if branch.Default {
-										branchID = branch.ID
-									}
-								}
-
-								resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
-								if err != nil {
-									return err
-								}
-								assert.Len(t, resp.Databases, 2)
-
-								for _, db := range resp.Databases {
-									if db.Name == "test" {
-										_, err := client.DeleteProjectBranchDatabase(ref.ID, branchID, db.Name)
-										if err != nil {
-											return err
-										}
-									}
-								}
-
-								return nil
-							},
-						),
-					},
-				},
-			})
-	})
-
-	t.Run("shall recreate database upon read if it was deleted outside of terraform", func(t *testing.T) {
-		projectName := newProjectName()
-		config := fmt.Sprintf(`resource "neon_project" "this" {name = "%s"}
-resource "neon_database" "this" {
-	project_id = neon_project.this.id
-	branch_id  = neon_project.this.default_branch_id
-	owner_name = neon_project.this.database_user
-	name       = "test"
-}`, projectName)
-		resource.Test(
-			t, resource.TestCase{
-				ProviderFactories: map[string]func() (*schema.Provider, error){
-					"neon": func() (*schema.Provider, error) {
-						return newAccTest(), nil
-					},
-				},
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check: resource.ComposeTestCheckFunc(
-							resource.TestCheckResourceAttr(
-								"neon_database.this",
-								"name", "test",
-							),
-							func(_ *terraform.State) error {
-								ref, err := readProjectInfo(client, projectName)
-								if err != nil {
-									return err
-								}
-								br, err := client.ListProjectBranches(ref.ID,
-									nil, nil, nil, nil, nil)
-								if err != nil {
-									return err
-								}
-
-								var branchID string
-								for _, branch := range br.Branches {
-									if branch.Default {
-										branchID = branch.ID
-									}
-								}
-
-								resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
-								if err != nil {
-									return err
-								}
-								assert.Len(t, resp.Databases, 2)
-
-								for _, db := range resp.Databases {
-									if db.Name == "test" {
-										_, err := client.DeleteProjectBranchDatabase(ref.ID, branchID, db.Name)
-										if err != nil {
-											return err
-										}
-									}
-								}
-
-								return nil
-							},
-						),
 					},
 					{
 						Config: config,
-						Check: resource.ComposeTestCheckFunc(
-							resource.TestCheckResourceAttr(
-								"neon_database.this",
-								"name", "test",
-							),
-							func(_ *terraform.State) error {
-								ref, err := readProjectInfo(client, projectName)
-								if err != nil {
-									return err
-								}
-								br, err := client.ListProjectBranches(ref.ID,
-									nil, nil, nil, nil, nil)
-								if err != nil {
-									return err
-								}
+						PreConfig: func() {
+							ref, err := readProjectInfo(client, projectName)
+							if err != nil {
+								panic(err)
+							}
+							br, err := client.ListProjectBranches(ref.ID,
+								nil, nil, nil, nil, nil)
+							if err != nil {
+								panic(err)
+							}
 
-								var branchID string
-								for _, branch := range br.Branches {
-									if branch.Default {
-										branchID = branch.ID
+							for _, branch := range br.Branches {
+								if branch.Default {
+									_, err := client.DeleteProjectBranchDatabase(ref.ID, branch.ID,
+										"test")
+									if err != nil {
+										panic(err)
 									}
 								}
-
-								resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
-								if err != nil {
-									return err
-								}
-								assert.Len(t, resp.Databases, 2)
-
-								return nil
-							},
-						),
-					},
-				},
-			})
-	})
-
-	t.Run("shall recreate database upon update if it was deleted outside of terraform", func(t *testing.T) {
-		projectName := newProjectName()
-		resource.Test(
-			t, resource.TestCase{
-				ProviderFactories: map[string]func() (*schema.Provider, error){
-					"neon": func() (*schema.Provider, error) {
-						return newAccTest(), nil
-					},
-				},
-				Steps: []resource.TestStep{
-					{
-						Config: fmt.Sprintf(`resource "neon_project" "this" {name = "%s"}
-resource "neon_database" "this" {
-	project_id = neon_project.this.id
-	branch_id  = neon_project.this.default_branch_id
-	owner_name = neon_project.this.database_user
-	name       = "foo"
-}`, projectName),
-						Check: resource.ComposeTestCheckFunc(
-							resource.TestCheckResourceAttr(
-								"neon_database.this",
-								"name", "foo",
-							),
-							func(_ *terraform.State) error {
-								ref, err := readProjectInfo(client, projectName)
-								if err != nil {
-									return err
-								}
-
-								br, err := client.ListProjectBranches(ref.ID,
-									nil, nil, nil, nil, nil)
-								if err != nil {
-									return err
-								}
-
-								var branchID string
-								for _, branch := range br.Branches {
-									if branch.Default {
-										branchID = branch.ID
-									}
-								}
-
-								resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
-								if err != nil {
-									return err
-								}
-								assert.Len(t, resp.Databases, 2)
-
-								for _, db := range resp.Databases {
-									if db.Name == "foo" {
-										_, err := client.DeleteProjectBranchDatabase(ref.ID, branchID, db.Name)
-										if err != nil {
-											return err
-										}
-									}
-								}
-
-								return nil
-							},
-						),
-					},
-					{
-						Config: fmt.Sprintf(`resource "neon_project" "this" {name = "%s"}
-resource "neon_database" "this" {
-	project_id = neon_project.this.id
-	branch_id  = neon_project.this.default_branch_id
-	owner_name = neon_project.this.database_user
-	name       = "bar"
-}`, projectName),
-						Check: resource.ComposeTestCheckFunc(
-							resource.TestCheckResourceAttr(
-								"neon_database.this",
-								"name", "bar",
-							),
-							func(_ *terraform.State) error {
-								ref, err := readProjectInfo(client, projectName)
-								if err != nil {
-									return err
-								}
-
-								br, err := client.ListProjectBranches(ref.ID,
-									nil, nil, nil, nil, nil)
-								if err != nil {
-									return err
-								}
-
-								var branchID string
-								for _, branch := range br.Branches {
-									if branch.Default {
-										branchID = branch.ID
-									}
-								}
-
-								resp, err := client.ListProjectBranchDatabases(ref.ID, branchID)
-								if err != nil {
-									return err
-								}
-								assert.Len(t, resp.Databases, 2)
-
-								var found bool
-								for _, db := range resp.Databases {
-									if db.Name == "bar" {
-										found = true
-									}
-								}
-								assert.Truef(t, found, "database 'bar' is expected to be found after recreation")
-								return nil
-							},
-						),
+							}
+						},
+						Destroy: true,
 					},
 				},
 			})
